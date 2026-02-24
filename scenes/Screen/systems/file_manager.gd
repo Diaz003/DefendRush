c2157460f62a9076b8a2d19b8c4a4cc101e1dae3
Alfonso Diaz Morales <166803966+Diaz003@users.noreply.github.com>
2026-02-24 13:13:25 +0100

    fix(file_manager): safeguard get_tree() against null during instancing

--- scenes/Screen/systems/file_manager.gd
+++ scenes/Screen/systems/file_manager.gd
@@ -2,7 +2,11 @@ extends "res://scenes/Screen/templates/template.gd"
 
 func _notification(what: int) -> void:
 	if what == NOTIFICATION_VISIBILITY_CHANGED:
-		if not is_visible_in_tree():
-			var screen_node = get_tree().root.get_node_or_null("Desktop")
-			if screen_node and "can_delete_files" in screen_node:
-				screen_node.can_delete_files = false
+		if not is_inside_tree():
+			return
+		if not visible:
+			var tree = get_tree()
+			if tree:
+				var screen_node = tree.root.get_node_or_null("Desktop")
+				if screen_node and "can_delete_files" in screen_node:
+					screen_node.can_delete_files = false
