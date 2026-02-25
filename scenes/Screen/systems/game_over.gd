5c331ec7edde0a2cd4fb97f56b10d9651409b848
Alfonso Diaz Morales <166803966+Diaz003@users.noreply.github.com>
2026-02-25 14:03:06 +0100

    Delete scenes directory

--- scenes/Screen/systems/game_over.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-func _on_restart_pressed() -> void:
-	get_tree().reload_current_scene()
-	
-func _on_menu_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/Menu/Menu.tscn")
